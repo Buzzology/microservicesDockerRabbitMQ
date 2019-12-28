322b9416b892c00b2834f6d9b641ac9cfa9fc895
Chris Owens <chrisowens@live.com.au>
2019-12-29 00:42:43 +1000

    [FEATURE] Add azure key vault and dynamic config

--- publisher_api/Program.cs
+++ publisher_api/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace publisher_api
 {
@@ -17,10 +14,42 @@ namespace publisher_api
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+
             Host.CreateDefaultBuilder(args)
+
+                // Added for azure key vault using eShopContainers as an example
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    GetConfiguration(config);
+                })
+
+                // Pre-existing
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        private static void GetConfiguration(IConfigurationBuilder builder)
+        {
+            // Add appsetting files and override with local settings if available
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Retrieve config from azure key vault if it's enabled
+            var config = builder.Build();
+            if (config.GetValue<bool>("azure-key-vault:enabled", false))
+            {
+                builder.AddAzureKeyVault(
+                    $"https://{config["azure-key-vault:name"]}.vault.azure.net/",
+                    config["azure-key-vault:client-id"],
+                    config["azure-key-vault:client-secret"]);
+            }
+
+            builder.Build();
+        }
     }
 }
